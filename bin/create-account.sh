6636068b9bb964cd8ea1af95d1df13ab696542fd
Yohei YASUKAWA <yasulab@gmail.com>
2012-03-19 23:56:12 -0700

    Change create-account.sh: mkdir -> mkdir -p

--- bin/create-account.sh
+++ bin/create-account.sh
@@ -8,8 +8,8 @@ else
 	exit 1
 	#echo "$1 already exists.\n"
     else
-	mkdir $RACKSCHOOL/usr/$1
-	mkdir $RACKSCHOOL/usr/$1/tmp
+	mkdir -p $RACKSCHOOL/usr/$1
+	mkdir -p $RACKSCHOOL/usr/$1/tmp
 	cp -r $RACKSCHOOL/src/learn-ruby $RACKSCHOOL/usr/$1/
 	#echo "Successfully created a user '$1'\n"
 	exit 1
